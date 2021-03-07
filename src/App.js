b000ccbe27469fdb949f5746ee161f5eb2a4c174
Kyle J Czech <kyle@kyleczech.com>
2021-03-07 00:25:20 -0600

    test barchart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { BarChart } from 'reaviz';
+
+export const data = [
+  {
+    key: 'Phishing Attack',
+    data: 10
+  },
+  {
+    key: 'IDS',
+    data: 14
+  },
+  {
+    key: 'Malware',
+    data: 5
+  },
+  {
+    key: 'DLP',
+    data: 18
+  }
+];
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +40,9 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+    <div style={{ margin: '55px', textAlign: 'center' }}>
+      <BarChart width={350} height={250} data={data} />
     </div>
   );
 }
